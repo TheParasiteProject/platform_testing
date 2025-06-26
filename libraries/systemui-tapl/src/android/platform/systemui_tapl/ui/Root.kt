a3e2006ba56c7dbe324dc0906d7957c090a9b6ed
Justin Weir <justinweir@google.com>
2025-06-26 07:03:20 -0700

    Revert "Revert "Replace split shade with dual shade when scenes ..."
    Revert submission 34230790-revert-34207253-splitShadeToDualShade-ZHWXELYKBV
    
    Reason for revert: Roll forward with test fixes
    
    Reverted changes: /q/submissionid:34230790-revert-34207253-splitShadeToDualShade-ZHWXELYKBV
    
    Change-Id: Ifd6c257cce0069bba1721c624d50b3bebbab6f8f

--- libraries/systemui-tapl/src/android/platform/systemui_tapl/ui/Root.kt
+++ libraries/systemui-tapl/src/android/platform/systemui_tapl/ui/Root.kt
@@ -94,7 +94,7 @@ class Root private constructor(val displayId: Int = DEFAULT_DISPLAY) {
     /** Opens the notification shade via two fingers wipe. */
     fun openNotificationShadeViaTwoFingersSwipe(): NotificationShade {
         assertWithMessage(
-            "two finger swipe is only supported on the default display, because it relies on " +
+                "two finger swipe is only supported on the default display, because it relies on " +
                     "UiObject#performTwoPointerGesture, and UiObject has no concept of displayId " +
                     "(unlike UiObject2)"
             )
@@ -195,7 +195,7 @@ class Root private constructor(val displayId: Int = DEFAULT_DISPLAY) {
         return NotificationShade(displayId)
     }
 
-    private val footerSelector = sysuiResSelector("qs_footer_actions", displayId)
+    private val qsSelector = sysuiResSelector("quick_settings_panel", displayId)
 
     private val notificationSwipeX: Float
         get() = uiDevice.getDisplayWidth(displayId) / 4f
@@ -238,7 +238,7 @@ class Root private constructor(val displayId: Int = DEFAULT_DISPLAY) {
     ) {
         if (assertIsHunState) {
             assertWithMessage(
-                "HUN state Assertion usage error: Notification: ${identity.title} " +
+                    "HUN state Assertion usage error: Notification: ${identity.title} " +
                         "| You can only assert the HUN State of a notification that has an action " +
                         "button."
                 )
@@ -266,9 +266,9 @@ class Root private constructor(val displayId: Int = DEFAULT_DISPLAY) {
         val device = uiDevice
         device.openQuickSettings()
         // Quick Settings isn't always open when this is complete. Explicitly wait for the Quick
-        // Settings footer to make sure that the buttons are accessible when the bar is open and
+        // Settings panel to make sure that the buttons are accessible when the bar is open and
         // this call is complete.
-        footerSelector.assertVisible()
+        qsSelector.assertVisible()
         // Wait an extra bit for the animation to complete. If we return to early, future callers
         // that are trying to find the location of the footer will get incorrect coordinates
         device.waitForIdle(LONG_TIMEOUT)
